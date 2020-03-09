8ed72736265d0309c13bfd88c80c1023199cb1da
GarethSanashee <gareth.sanashee@appsinthesky.com>
2020-03-09 13:16:33 +0000

    filtering basics

--- src/app/datatable/datatable.component.ts
+++ src/app/datatable/datatable.component.ts
@@ -2,7 +2,8 @@ import { AfterViewInit, Component, OnInit, ViewChild } from '@angular/core';
 import { MatPaginator } from '@angular/material/paginator';
 import { MatSort } from '@angular/material/sort';
 import { MatTable } from '@angular/material/table';
-import { DatatableDataSource, DatatableItem } from './datatable-datasource';
+import { DatatableDataSource } from './datatable-datasource';
+import {MatTableDataSource} from '@angular/material';
 
 @Component({
   selector: 'app-datatable',
@@ -13,13 +14,37 @@ export class DatatableComponent implements AfterViewInit, OnInit {
   @ViewChild(MatPaginator, {static: false}) paginator: MatPaginator;
   @ViewChild(MatSort, {static: false}) sort: MatSort;
   @ViewChild(MatTable, {static: false}) table: MatTable<DatatableItem>;
-  dataSource: DatatableDataSource;
+  //dataSource: DatatableDataSource;
+   // TODO: replace this with real data from your application
+   EXAMPLE_DATA: DatatableItem[] = [
+    {id: 1, name: 'Hydrogen'},
+    {id: 2, name: 'Helium'},
+    {id: 3, name: 'Lithium'},
+    {id: 4, name: 'Beryllium'},
+    {id: 5, name: 'Boron'},
+    {id: 6, name: 'Carbon'},
+    {id: 7, name: 'Nitrogen'},
+    {id: 8, name: 'Oxygen'},
+    {id: 9, name: 'Fluorine'},
+    {id: 10, name: 'Neon'},
+    {id: 11, name: 'Sodium'},
+    {id: 12, name: 'Magnesium'},
+    {id: 13, name: 'Aluminum'},
+    {id: 14, name: 'Silicon'},
+    {id: 15, name: 'Phosphorus'},
+    {id: 16, name: 'Sulfur'},
+    {id: 17, name: 'Chlorine'},
+    {id: 18, name: 'Argon'},
+    {id: 19, name: 'Potassium'},
+    {id: 20, name: 'Calcium'},
+  ];
+  dataSource = new MatTableDataSource(this.EXAMPLE_DATA);
 
   /** Columns displayed in the table. Columns IDs can be added, removed, or reordered. */
   displayedColumns = ['User', 'Purchased', 'Used'];
 
   ngOnInit() {
-    this.dataSource = new DatatableDataSource();
+    //this.dataSource = new DatatableDataSource();
   }
 
   ngAfterViewInit() {
@@ -27,4 +52,13 @@ export class DatatableComponent implements AfterViewInit, OnInit {
     this.dataSource.paginator = this.paginator;
     this.table.dataSource = this.dataSource;
   }
+
+  public doFilter = (value: string) => {
+    this.dataSource.filter = value.trim().toLocaleLowerCase();
+  }
+}
+
+interface DatatableItem {
+  name: string;
+  id: number;
 }
